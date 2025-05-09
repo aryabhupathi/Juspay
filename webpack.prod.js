558f8f47dfb5e164d39a33b3100bc557e98b50c0
Arya Bhupathi <bhupathikonduru@gmail.com>
2025-05-09 16:52:27 +0530

    deploy

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,20 +1,68 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+// const { merge } = require("webpack-merge");
+// const common = require("./webpack.common.js");
+// const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
-process.env["NODE_ENV"] = "production";
+// process.env["NODE_ENV"] = "production";
 
-module.exports = merge([
-  common,
-  {
-    mode: "production",
-    optimization: {
-      minimize: true,
-      minimizer: [
-        // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
-        // `...`,
-        new CssMinimizerPlugin(),
-      ],
-    },
+// module.exports = merge([
+//   common,
+//   {
+//     mode: "production",
+//     optimization: {
+//       minimize: true,
+//       minimizer: [
+//         // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+//         // `...`,
+//         new CssMinimizerPlugin(),
+//       ],
+//     },
+//   },
+// ]);
+
+
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: "./src/index.js", // renamed entry point to "main"
   },
-]);
+  output: {
+    filename: "[name].bundle.js", // main.bundle.js
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    publicPath: "/", // important for routing and asset loading
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css", // main.css
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "public", // source folder
+          to: "", // destination (dist root)
+        },
+      ],
+    }),
+  ],
+};
